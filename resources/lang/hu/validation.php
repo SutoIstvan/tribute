a828d19c8496d10a1f492d89f088cebe62247554
SutoIstvan <isutoi@gmail.com>
2025-02-24 19:15:52 +0200

    add hu lang

---
+++ resources/lang/hu/validation.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'required' => 'A(z) :attribute mező kitöltése kötelező.',
+
+    'attributes' => [
+        'name' => 'név',
+        'birth_date' => 'születési dátum',
+        'death_date' => 'halálozási dátum',
+        'biography' => 'életrajz',
+        'photo' => 'emlékkép',
+    ],
+];
